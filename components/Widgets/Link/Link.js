7bea8648f2bc6c361077e8ffb85cd3e825e3c932
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-05-31 13:48:19 +0100

    update: active link setup

--- components/Widgets/Link/Link.js
+++ components/Widgets/Link/Link.js
@@ -1,19 +1,22 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const LinkList = props => (
-  <li>
-    <Link href={`${props.url}`}>
-      <a
-        className={`block py-2 sm:pr-4 no-underline ${
-          !props.noHover &&
-          "hover:text-black dark:hover:text-white hover:font-normal duration-500 ease-in-out hover:no-underline transition"
-        } sm:pl-3 capitalize md:bg-transparent md:p-0 font-normal`}
-        aria-current="page"
-      >
-        {props.name}
-      </a>
-    </Link>
-  </li>
-);
+const LinkList = props => {
+  const router = useRouter();
+  return (
+    <li>
+      <Link href={`${props.url}`}>
+        <a
+          className={`block py-2 sm:pr-4 no-underline ${
+            router.pathname == props.url ? "text-second font-medium" : ""
+          } hover:text-second hover:font-normal duration-500 ease-in-out hover:no-underline transition sm:pl-3 capitalize md:bg-transparent md:p-0`}
+          aria-current="page"
+        >
+          {props.name}
+        </a>
+      </Link>
+    </li>
+  );
+};
 
 export default LinkList;
