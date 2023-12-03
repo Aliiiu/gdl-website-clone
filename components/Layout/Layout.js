64301f774b34d16fb90e3f2c336177343aeccb4f
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-12-03 19:52:05 +0100

    logic to show only content

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,20 +2,32 @@ import { PageContainer, JoinClubContainer } from "./layout.style";
 import Topbar from "../Topbar/Topbar";
 import Footer from "../Footer/Footer";
 import JoinTheClub from "../PageSections/JoinTheClub";
+import { useRouter } from "next/router";
 
-const Layout = ({ children, ...props }) => (
-  <div className="">
-    <Topbar />
-    <main>
-      <PageContainer>
-        <div className="w-full h-full">{children}</div>
-      </PageContainer>
-    </main>
-    <JoinClubContainer id="join-club" className="softGradient">
-      <JoinTheClub />
-    </JoinClubContainer>
-    <Footer />
-  </div>
-);
+const Layout = ({ children, ...props }) => {
+  const route = useRouter();
+
+  const { hideLayout } = route.query;
+  return (
+    <div className="">
+      {hideLayout ? "" : <Topbar />}
+      <main>
+        <PageContainer>
+          <div className="w-full h-full">{children}</div>
+        </PageContainer>
+      </main>
+      {hideLayout ? (
+        ""
+      ) : (
+        <>
+          <JoinClubContainer id="join-club" className="softGradient">
+            <JoinTheClub />
+          </JoinClubContainer>
+          <Footer />
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Layout;
--- pages/terms-of-use.js
+++ pages/terms-of-use.js
@@ -3,8 +3,12 @@ import Terms from "../components/PageSections/Terms";
 import Faq from "../components/PageSections/Faq";
 import { makeRequest } from "../apiCalls/requestHandler";
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 const TermsOfUse = ({ termsContent }) => {
+  const route = useRouter();
+
+  const { hideLayout } = route.query;
   return (
     <div className="relative">
       <Head>
@@ -13,12 +17,16 @@ const TermsOfUse = ({ termsContent }) => {
       <section className="container xl:max-w-[1000px] px-4 md:px-10 xl:px-20 mx-auto">
         <Terms />
       </section>
-      <section
-        id="faq"
-        className="lg:px-6 lg:w-4/6 mx-auto py-16 md:py-36 w-full"
-      >
-        <Faq />
-      </section>
+      {hideLayout ? (
+        ""
+      ) : (
+        <section
+          id="faq"
+          className="lg:px-6 lg:w-4/6 mx-auto py-16 md:py-36 w-full"
+        >
+          <Faq />
+        </section>
+      )}
     </div>
   );
 };
