cd3fa06fee094450955b592f688387a1f83909ab
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-06-20 14:30:33 +0100

    reverting env variables

--- apiCalls/requestHandler.js
+++ apiCalls/requestHandler.js
@@ -1,7 +1,7 @@
 // const response = await fetch("https://test-site.gdlapi.com.ng/pages/home/hero");
 // const data = await response.json();
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL; // suppose to come from the env file
+const baseURL = "http://62.173.45.205:30013"; // suppose to come from the env file
 
 // const baseURL = "http://165.22.115.227";
 
