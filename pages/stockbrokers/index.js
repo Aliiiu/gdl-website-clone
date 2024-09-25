753cf1d32e937be608c1674b60b08a4372604d66
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-09-25 15:07:31 +0100

    update stockbroker page layout

--- pages/stockbrokers/index.js
+++ pages/stockbrokers/index.js
@@ -47,18 +47,18 @@ const Stockbrokers = () => {
       </div>
       <section
         id="management-team"
-        className="lg:px-6 md:w-5/6 lg:w-4/6 mx-auto py-16 2xl:py-20 w-full"
+        className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
       >
         <Management data={stockbrokersManagement} notClickable={true} />
       </section>
       <section
         id="board-of-directors"
-        className="lg:px-6 md:w-5/6 lg:w-4/6 mx-auto py-16 2xl:py-20 w-full"
+        className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
       >
         <Boards data={stockbrokersBOD} notClickable={true} />
       </section>
 
-      <section id="products" className="py-16 2xl:py-20">
+      <section id="products" className="py-16 md:py-24 xl:py-36">
         <SectionWrapper className="container px-4 xl:px-28 mx-auto">
           <div className="text-center md:text-center">
             <h1 className="title mb-[.75rem]">Products</h1>
