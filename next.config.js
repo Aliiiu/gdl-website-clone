fa4af5cf325f56d97e2921b03dafaa8df8ced94c
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-12-16 14:00:04 +0100

    force code to be deployed on later node version

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        crypto: require.resolve("crypto-browserify"),
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
