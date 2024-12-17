db8905eb007ba5d726764e864e13449e27021a43
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-12-17 18:46:39 +0100

    add metadata to finance and leasing page

--- app/new/finance-and-leasing/page.tsx
+++ app/new/finance-and-leasing/page.tsx
@@ -1,7 +1,12 @@
+import { generatePageMetadata } from "@/app/utils/metadata";
 import BoardTeamSection from "./_components/board-team";
 import Hero from "./_components/hero";
 import ManagementTeamSection from "./_components/management-team";
 import Products from "./_components/products";
+
+export const generateMetadata = () => {
+	return generatePageMetadata({ title: "Finance and Leasing" });
+};
 
 export default function Page() {
 	return (
