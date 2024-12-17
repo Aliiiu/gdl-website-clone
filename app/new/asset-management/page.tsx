c673a994851acfcb71c19c6ea1747c827038df39
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-12-17 18:47:31 +0100

    add metadata to asset management page

--- app/new/asset-management/page.tsx
+++ app/new/asset-management/page.tsx
@@ -1,7 +1,12 @@
+import { generatePageMetadata } from "@/app/utils/metadata";
 import BoardTeamSection from "./_components/board-team";
 import Hero from "./_components/hero";
 import ManagementTeamSection from "./_components/management-team";
 import Products from "./_components/products";
+
+export const generateMetadata = () => {
+	return generatePageMetadata({ title: "Asset Management" });
+};
 
 export default function Page() {
 	return (
