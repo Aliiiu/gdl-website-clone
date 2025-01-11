a24d356962a72b50be3cd37fbb9ebebc92f4cb4e
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-01-11 22:36:21 +0100

    done resolving stockbrokers page

--- app/stockbrokers/_components/board-team.tsx
+++ app/stockbrokers/_components/board-team.tsx
@@ -1,46 +1,45 @@
-import BoardTeam from "@/app/components/board-team";
-import React from "react";
+import BoardTeam from "@/components/board-team";
 
 const BoardTeamSection = () => {
-	return (
-		<section
-			id="board-of-directors"
-			className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
-		>
-			<BoardTeam data={stockbrokersBOD} notClickable={true} />
-		</section>
-	);
+  return (
+    <section
+      id="board-of-directors"
+      className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
+    >
+      <BoardTeam data={stockbrokersBOD} notClickable={true} />
+    </section>
+  );
 };
 
 export default BoardTeamSection;
 
 const stockbrokersBOD = [
-	{
-		id: 1,
-		image_url: "/Images/kolawole-Ayeye.jpeg",
-		staff_name: "Kolawole Ayeye - Chairman",
-	},
-	{
-		id: 2,
-		image_url: "/Images/Esther-new.webp",
-		staff_initials: "TAS",
-		staff_name: "Chinyere Esther Ndife – Managing Director",
-	},
-	{
-		id: 3,
-		image_url: "/Images/Oyekemi-Ayeye.jpeg",
-		staff_name: "Oyekemi Ayeye - Non-Executive Director",
-	},
-	{
-		id: 4,
-		image_url: "/Images/Josephine-Ernest-Ikechukwu.jpeg",
-		staff_name: "Josephine Ernest Ikechukwu - Non-Executive Director",
-	},
-	{
-		id: 5,
-		image_url: "/Images/Cyril.webp",
-		staff_initials: "CBI",
-		staff_name:
-			"Cyril Baba-Oyathelemi Ikazoboh - Non-Executive (Independent Director)",
-	},
+  {
+    id: 1,
+    image_url: "/Images/kolawole-Ayeye.jpeg",
+    staff_name: "Kolawole Ayeye - Chairman",
+  },
+  {
+    id: 2,
+    image_url: "/Images/Esther-new.webp",
+    staff_initials: "TAS",
+    staff_name: "Chinyere Esther Ndife – Managing Director",
+  },
+  {
+    id: 3,
+    image_url: "/Images/Oyekemi-Ayeye.jpeg",
+    staff_name: "Oyekemi Ayeye - Non-Executive Director",
+  },
+  {
+    id: 4,
+    image_url: "/Images/Josephine-Ernest-Ikechukwu.jpeg",
+    staff_name: "Josephine Ernest Ikechukwu - Non-Executive Director",
+  },
+  {
+    id: 5,
+    image_url: "/Images/Cyril.webp",
+    staff_initials: "CBI",
+    staff_name:
+      "Cyril Baba-Oyathelemi Ikazoboh - Non-Executive (Independent Director)",
+  },
 ];
--- app/stockbrokers/_components/management-team.tsx
+++ app/stockbrokers/_components/management-team.tsx
@@ -1,36 +1,35 @@
-import ManagementTeam from "@/app/components/management-team";
-import React from "react";
+import ManagementTeam from "@/components/management-team";
 
 const ManagementTeamSection = () => {
-	return (
-		<section
-			id="management-team"
-			className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
-		>
-			<ManagementTeam data={stockbrokersManagement} notClickable={true} />
-		</section>
-	);
+  return (
+    <section
+      id="management-team"
+      className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
+    >
+      <ManagementTeam data={stockbrokersManagement} notClickable={true} />
+    </section>
+  );
 };
 
 export default ManagementTeamSection;
 
 const stockbrokersManagement = [
-	{
-		id: 2,
-		image_url: "/Images/Esther-new.webp",
-		staff_initials: "ED",
-		staff_name: "Esther Ndife – DMD",
-	},
-	{
-		id: 4,
-		image_url: "/Images/Stephen-new.webp",
-		staff_initials: "SF",
-		staff_name: "Stephen Fidelis – Chief Financial Officer",
-	},
-	{
-		id: 3,
-		image_url: "",
-		staff_initials: "OA",
-		staff_name: "Olufunmilola Adebowale – Chief Research Officer",
-	},
+  {
+    id: 2,
+    image_url: "/Images/Esther-new.webp",
+    staff_initials: "ED",
+    staff_name: "Esther Ndife – DMD",
+  },
+  {
+    id: 4,
+    image_url: "/Images/Stephen-new.webp",
+    staff_initials: "SF",
+    staff_name: "Stephen Fidelis – Chief Financial Officer",
+  },
+  {
+    id: 3,
+    image_url: "",
+    staff_initials: "OA",
+    staff_name: "Olufunmilola Adebowale – Chief Research Officer",
+  },
 ];
