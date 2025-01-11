b5fa7b1a350eef782809639be1f210d22cfa70d7
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-01-11 21:11:21 +0100

    done resolving about import errors in about page

--- app/about/_components/board.tsx
+++ app/about/_components/board.tsx
@@ -1,16 +1,16 @@
-import BoardTeam from "@/app/components/board-team";
+import BoardTeam from "@/components/board-team";
 import { getBoardDetails } from "../actions";
 
 const Board = async () => {
-	const boardDetail = await getBoardDetails();
-	return (
-		<section
-			id="board-of-directors"
-			className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
-		>
-			<BoardTeam data={boardDetail} />
-		</section>
-	);
+  const boardDetail = await getBoardDetails();
+  return (
+    <section
+      id="board-of-directors"
+      className="container lg:px-6 md:w-5/6 lg:w-4/6 mx-auto pt-16 md:pt-24 xl:pt-36 w-full"
+    >
+      <BoardTeam data={boardDetail} />
+    </section>
+  );
 };
 
 export default Board;
