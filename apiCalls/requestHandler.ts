2ef15634df6faa7be3f38d5de91ae6f9983058f7
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-02-23 23:56:28 +0100

    remove unused files

---
+++ apiCalls/requestHandler.ts
@@ -0,0 +1,28 @@
+const baseURL = "https://site.housemoni.net"; // TODO: Move to environment variables
+
+type RequestMethod = "GET" | "POST" | "PUT" | "DELETE" | "PATCH";
+
+export const makeRequest = async <T>(
+	url: string,
+	data: Record<string, any> | null = null,
+	token: string | null = null,
+	method_type: RequestMethod | null = null,
+) => {
+	const response = await fetch(`${baseURL}${url}`, {
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			...(token && { Authorization: `Bearer ${token}` }),
+		},
+		method: method_type ?? (data == null ? "GET" : "POST"),
+		body: data == null ? null : JSON.stringify(data),
+	});
+
+	if (response.ok) {
+		const responseData = await response.json();
+		return { data: responseData };
+	}
+
+	const { response_description } = await response.json();
+	throw new Error(response_description);
+};
