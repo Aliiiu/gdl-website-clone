d167b09e3a67e2a973b8bdfa9d3b739d65649fd5
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-03-07 15:13:20 +0100

    add og image

--- utils/metadata.ts
+++ utils/metadata.ts
@@ -1,29 +1,37 @@
 type MetadataProps = {
-  title?: string;
+	title?: string;
 };
 
 export const generatePageMetadata = ({ title }: MetadataProps = {}) => {
-  const defaultTitle = "GDL | Build Wealth Comfortably";
-  const template = "%s | GDL";
-  const baseDescription =
-    "A leading diversified financial institution creating wealth and transforming society";
+	const defaultTitle = "GDL | Build Wealth Comfortably";
+	const template = "%s | GDL";
+	const baseDescription =
+		"A leading diversified financial institution creating wealth and transforming society";
 
-  const pageTitle = title ? template.replace("%s", title) : defaultTitle;
+	const pageTitle = title ? template.replace("%s", title) : defaultTitle;
 
-  return {
-    title: pageTitle,
-    description: baseDescription,
-    openGraph: {
-      title: pageTitle,
-      description: baseDescription,
-      type: "website",
-      siteName: "gdl",
-    },
-    twitter: {
-      card: "summary_large_image",
-      title: pageTitle,
-      description: baseDescription,
-      creator: "@gdl",
-    },
-  };
+	return {
+		title: pageTitle,
+		description: baseDescription,
+		openGraph: {
+			title: pageTitle,
+			description: baseDescription,
+			type: "website",
+			siteName: "gdl",
+			images: [
+				{
+					url: "/og-image-default.png",
+					width: 900,
+					height: 430,
+					alt: baseDescription,
+				},
+			],
+		},
+		twitter: {
+			card: "summary_large_image",
+			title: pageTitle,
+			description: baseDescription,
+			creator: "@gdl",
+		},
+	};
 };
